4cd9b17a491fe882b0e505c9588fd328e2d9b70e
Wanderson Herênio Aragão Leite <wanderson.eq2011@gmail.com>
2024-12-26 18:47:29 -0300

    Add ChromeDriver configuration

--- src/scraping/utils/web_driver_factory.py
+++ src/scraping/utils/web_driver_factory.py
@@ -4,26 +4,28 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import os
 
+
 class WebDriverFactory:
     @staticmethod
     def create_driver(headless=False):
         chrome_options = Options()
         if headless:
             chrome_options.add_argument("--headless")
-            
+
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-infobars")
-        chrome_options.add_argument("--disable-browser-side-navigation")
-        chrome_options.add_argument("--disable-features=VizDisplayCompositor")
 
-        environment = os.getenv('ENVIRONMENT', 'local')
-        if environment == 'production':
-            chrome_service = Service('/usr/local/bin/chromedriver')
-            return webdriver.Chrome(service=chrome_service, options=chrome_options)
+        environment = os.getenv("ENVIRONMENT", "local")
+        if environment == "local":
+            return webdriver.Chrome(options=chrome_options)
         else:
-            return webdriver.Chrome(options=chrome_options)        
+            chrome_options.binary_location = os.environ.get(
+                "GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome"
+            )
+            chrome_driver_path = os.environ.get(
+                "CHROMEDRIVER_PATH", "/app/.chromedriver/bin/chromedriver"
+            )
+            service = Service(chrome_driver_path)
+            return webdriver.Chrome(service=service, options=chrome_options)
